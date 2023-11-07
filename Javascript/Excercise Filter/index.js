c62838ac1a6f29b4c8732721111cef19a2ee2fd1
mohammedfaizaansharief <mohdfaizaansharief@gmail.com>
2023-11-07 18:05:52 +0530

    asksi

--- Javascript/Excercise Filter/index.js
+++ Javascript/Excercise Filter/index.js
@@ -13,9 +13,34 @@ function newq(z){
 console.log(r)
 
 // 2
-let countries = ['INDIA','AMERICA','USA','FRANCE','ITALY'] 
-var t = countries.filter(fun4);
+let countries = ['INDIA','AMERICA','USA','FRANCE','ITALY','EGYPT'] 
+ var newCountries = countries.filter((val)=>{
+    return val.length < 6;
+ })
+ console.log(newCountries)
 
-function fun4(item1){
-    
-}
+ // 3
+ var countriesNew = countries.filter((s)=>{
+    return s.charAt()!="E"
+ })
+ console.log(countriesNew)
+
+ // 4
+ var products= [{product:'sugar',
+                price :4},   
+                {product:'milk',
+                price :10},
+                {product:'bread',
+                price :30},
+                {product:'roti',
+                price :4},
+                {product:'juice',
+                price :50},]
+
+ var newProducts = products.filter((item)=>{
+    return item.price < 10
+ })
+console.log(newProducts)
+
+// 5
+let employees = [{adnan:'50000',mansoor:'60000',Mohammed:'90000',Abdullah:'20000'}]
