6bd7048626e44785e0372b7f7961b8fe65736c39
mohammedfaizaansharief <mohdfaizaansharief@gmail.com>
2023-11-22 21:56:07 +0530

    indexaa

--- Javascript/EXERCISE -FETCH API/index.js
+++ Javascript/EXERCISE -FETCH API/index.js
@@ -1,11 +1,52 @@
+let container = document.querySelector('.container')
+let orderList = document.querySelector('.ordlist')
+// let table = document.querySelector('.table')
 
 fetch('https://restcountries.com/v2/all')
 .then((data)=>{
     return data.json()
 })
 .then((data1)=>{
-    console.log(data1[0])
+    console.log(data1)
+    let userData = data1
+    
+    // let newUserData = data1
+    // userData = data1
+    userData.map((e)=>{
+        console.log(e)
+    })
+    // container.innerHTML= `<li>Hi</li>`
+    // container.textContent
+    // orderList.innerHTML = `<li>Hi</li><li>Hi</li>`
+    
+    userData.map(element => {
+        let listval = document.createElement('li')
+        let langs = element.languages.map((l)=>{
+            return l.name
+        })
+        // let languages = element.languages
+        let
+        listval.innerHTML = `${element.name}
+                            ${element.capital}
+                            ${langs.toString()}
+                            ${element.population}`
+        
+        orderList.appendChild(listval)
+    });
+
+    var table = document.createElement("table")
+    table.setAttribute("id","myTable")
+    document.body.appendChild(table)
+
+    var y = document.createElement("tr")
+    y.setAttribute("id","myTR")
+    document.getElementById("myTable").appendChild(y)
+
+    var z = document.createAttribute("td")
+    z.
 })
+
+
 
 //
 // apicall('ht....',(data)=>{
