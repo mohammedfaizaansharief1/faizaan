e33853cd89beb0c5e4fe9355d8f23848d1ffd6d1
mohammedfaizaansharief <mohdfaizaansharief@gmail.com>
2023-11-28 22:00:03 +0530

    wasteV1

--- ZZzzz/script.js
+++ ZZzzz/script.js
@@ -1,24 +1,39 @@
 function addTask() {
-    const taskInput = document.getElementById('taskInput');
-    const taskList = document.getElementById('taskList');
-  
-    const taskText = taskInput.value.trim();
-    if (taskText !== '') {
-      const li = document.createElement('li');
-      li.textContent = taskText;
-  
-      const deleteButton = document.createElement('button');
-      deleteButton.textContent = '❌';
-      deleteButton.onclick = function() {
-        taskList.removeChild(li);
-      };
-  
-      li.appendChild(deleteButton);
-      taskList.appendChild(li);
-  
-      taskInput.value = '';
-    } else {
-      alert('Please enter a task.');
-    }
+  const taskInput = document.getElementById("taskInput");
+  const taskList = document.getElementById("taskList");
+
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "❌";
+    deleteButton.onclick = function () {
+      taskList.removeChild(li);
+    };
+
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+
+    const editButton = document.createElement("button");
+    editButton.textContent = "✎";
+    editButton.onclick = function () {
+      const newText = prompt("Edit task:", li.textContent);
+      if (newText !== null && newText.trim() !== "") {
+        li.textContent = newText.trim();
+        li.appendChild(deleteButton);
+        li.appendChild(editButton);
+      }
+    };
+
+    li.appendChild(editButton);
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+
+    // taskInput.value = "";
+  } else {
+    alert("Please enter a task.");
   }
-  
+}
