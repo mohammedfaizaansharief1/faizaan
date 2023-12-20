3f0eaef648adb1a5473e7da83d35bbecc504dea6
mohammedfaizaansharief <mohdfaizaansharief@gmail.com>
2023-12-20 22:00:02 +0530

    assssss

--- REACT/React_Project_3/vite-project/src/Child.jsx
+++ REACT/React_Project_3/vite-project/src/Child.jsx
@@ -1,10 +1,13 @@
 // import React from 'react'
 import './Child.css'
+import Left from './Left'
+import Right from './Right'
 
 export default function Child() {
   return (
     <div className='childContainer'>
-      
+      <Left />
+      <Right />
     </div>
   )
 }
