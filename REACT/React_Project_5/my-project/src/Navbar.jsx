5e57e11db76ab5d8e3a8866ee44c5a4dd0ae049a
mohammedfaizaansharief <mohdfaizaansharief@gmail.com>
2023-12-27 17:58:23 +0530

    asdyy

---
+++ REACT/React_Project_5/my-project/src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <div className="container bg-red-500 w-full">
+        <div className="logo">
+            logo
+        </div>
+        <div className="links">
+            <a href="a">ashd</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
