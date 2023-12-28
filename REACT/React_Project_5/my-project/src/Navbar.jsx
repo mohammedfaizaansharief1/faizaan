4c13dd95e834390aacf476561b6ebef129855f5e
mohammedfaizaansharief <mohdfaizaansharief@gmail.com>
2023-12-28 20:30:00 +0530

    asdttx

--- REACT/React_Project_5/my-project/src/Navbar.jsx
+++ REACT/React_Project_5/my-project/src/Navbar.jsx
@@ -7,6 +7,8 @@ function Navbar() {
       <div className="container bg-red-500  h-12 p-10 max-w-7xl">
         <div className="logo">
             logo
+            {/* <button className='text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800"'>Click</button>
+            <button className='bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>Click</button> */}
         </div>
         <div className="links">
             <a href="a">About</a>
