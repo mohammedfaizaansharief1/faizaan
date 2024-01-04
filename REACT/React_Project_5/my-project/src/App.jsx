cab8482fa90a7b1f81bde5bdcec6b260d867de19
mohammedfaizaansharief <mohdfaizaansharief@gmail.com>
2024-01-04 20:17:37 +0530

    asdsde

--- REACT/React_Project_5/my-project/src/App.jsx
+++ REACT/React_Project_5/my-project/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
     <>
     <Navbar/>
     <h1 className='bg-red-500'>Heading</h1>
-    
+    <Footer/>
     </>
   )
 }
