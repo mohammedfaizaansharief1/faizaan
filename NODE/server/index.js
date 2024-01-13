7ce7831e4aa70b597d0eb779ad023e31d70c561e
mohammedfaizaansharief <mohdfaizaansharief@gmail.com>
2024-01-13 22:05:13 +0530

    asdasd

--- NODE/server/index.js
+++ NODE/server/index.js
@@ -1,26 +1,31 @@
 const express = require('express')
 const app = express()
-// let fs = require('fs')
+let fs = require('fs')
+let data = fs.readFileSync('./index.html')
 
 const port = 3000
 // const port = 65535
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-  
-// })
-
-app.get('/profile/:username', (req, res) => {
-  const username = req.params.username
-  res.send("assalamualikum")
-  res.json.toString                                                                                                                                                                                                                                                                                              (username)
+app.get('/', (req, res) => {
+  // res.send(data)
+  res.send('get!')
   res.end()
 })
 
-app.post('/',(req,res)=>{
-  // console.log("req.body")
-  res.send("Hi")
-  res.end()
+// app.get('/profile/:username', (req, res) => {
+  //   const username = req.params.username
+  //   res.send("assalamualikum")
+  //   // res.json.toString                                                                                                                                                                                                                                                                                              (username)
+  //   res.end()
+  // })
+  
+  app.post('/',(req,res)=>{
+    // console.log("req.body")
+    // res.send("post")
+    const {name} = req.body
+    res.send(name)
+    // res.send(`name: ${name}`)
+    res.end()
   // res.query()
 })
 app.put('/',(req,res)=>{
