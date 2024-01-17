3bb788aeb0182a6e0088cea1d168b5737617789c
mohammedfaizaansharief <mohdfaizaansharief@gmail.com>
2024-01-17 08:18:45 +0530

    initial_connect_to_express

---
+++ N_React_Node/my-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, nihil. Omnis distinctio qui, in voluptatum autem, ab, quidem vero quos voluptatem eius praesentium vitae ad ipsam minus soluta voluptas placeat.</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
